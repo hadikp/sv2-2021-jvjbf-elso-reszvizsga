322bef45964fbf08ecf0bdd64e5bf5b066335aa8
hadikp <hp20001025@gmail.com>
2021-11-09 14:19:34 +0100

    init

---
+++ sv2-2021-jvjbf-elso-reszvizsga/src/main/java/stringmanipulation/StringManipulationMain.java
@@ -0,0 +1,7 @@
+package stringmanipulation;
+
+public class StringManipulationMain {
+        StringManipulation stringManipulation = new StringManipulation();
+
+        System.out.println(stringManipulation.everyEvenCharacter("This is a String")); //Ti saSrn
+}
